98a2808e4b34f0413eac92c2f9b298ff060af7d9
Emily Grace Seville <42812113+EmilySeville7cfg@users.noreply.github.com>
2021-12-07 12:39:34 +0000

    Add CommandLineParser package example

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,12 +1,32 @@
 using System;
+using CommandLine;
 
 namespace Program
 {
+  public class Options
+  {
+    [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+    public bool Verbose { get; set; }
+  }
+
   internal class Program
   {
     private static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Parser.Default.ParseArguments<Options>(args)
+        .WithParsed<Options>(o =>
+        {
+          if (o.Verbose)
+          {
+            Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+            Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+          }
+          else
+          {
+            Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+            Console.WriteLine("Quick Start Example!");
+          }
+        });
     }
   }
 }
